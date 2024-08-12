78cf467b79da05307dcc0f394ad95ba4877a01fd
0x676e67 <gngppz@gmail.com>
2024-08-12 08:24:00 +0800

    Export frame `PseudoOrder` settings

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -51,9 +51,8 @@ mod window_update;
 pub use self::data::Data;
 pub use self::go_away::GoAway;
 pub use self::head::{Head, Kind};
-pub(crate) use self::headers::PseudoOrder;
 pub use self::headers::{
-    parse_u64, Continuation, Headers, Pseudo, PushPromise, PushPromiseHeaderError,
+    parse_u64, Continuation, Headers, Pseudo, PseudoOrder, PushPromise, PushPromiseHeaderError,
 };
 pub use self::ping::Ping;
 pub use self::priority::{Priority, StreamDependency};
