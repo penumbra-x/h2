631bfec717dfa7cd80b645838183a6508d289cd2
0x676e67 <gngppz@gmail.com>
2024-12-31 15:32:29 +0800

    update

--- src/client.rs
+++ src/client.rs
@@ -1691,7 +1691,7 @@ impl Peer {
 
         if let Some(ref priority) = priority {
             if let Some(last_priority) = priority.last() {
-                if last_priority.stream_id() != id {
+                if last_priority.stream_id().next_id()? != id {
                     return Err(UserError::OverflowedStreamId.into());
                 }
             }
